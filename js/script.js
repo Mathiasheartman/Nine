65fa6a7953164f49e82a831f32d711ac168457fc
Mathias Hearman <161854301+Mathiasheartman@users.noreply.github.com>
2024-09-05 16:31:35 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const countDownDate = new Date("sep 20, 2024 00:00:00").getTime();
+
+const interval = setInterval(() => {
+  const now = new Date().getTime();
+  const duration = countDownDate - now;
+  
+  if (duration < 0) {
+    clearInterval(interval);
+    updateDuration(0);
+    return;
+  }
+  
+  updateDuration(duration);
+  }, 1000);
+
+function updateDuration(duration)  {
+  days = Math.floor(duration / (1000 * 60 * 60 * 24));
+  hours = Math.floor((duration % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  minutes = Math.floor((duration % (1000 * 60 * 60)) / (1000 * 60));
+  seconds = Math.floor((duration % (1000 * 60)) / 1000);
+  
+  
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+}
